4c2cd6cf4e0dd0bac720049f267d445909cf6e8e
minphyothaw <minphyothaw88@gmail.com>
2025-10-02 16:09:51 +0630

    update the filelink

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,7 +3,7 @@ import { Github, Linkedin, Mail, Download } from 'lucide-react';
 export default function Hero() {
   const handleDownloadResume = () => {
      const link = document.createElement('a');
-    link.href = '/min_phyo_thaw_cv.pdf';  
+    link.href = 'public/min_phyo_thaw_cv.pdf';  
     link.download = 'Min_Phyo_Thaw_Resume.pdf';
     document.body.appendChild(link); 
     link.click();
